3fc94864a9d57973cb78477edc47a90887aa0ffb
michaelJwilson <mjwilson@lbl.gov>
2021-03-11 18:15:16 -0800

    Correct env. vars.

--- gold/secondary/run_scndtarget.sh
+++ gold/secondary/run_scndtarget.sh
@@ -1,17 +1,21 @@
-export GAIA_DIR=/project/projectdirs/cosmo/data/gaia/dr2/
+export DUST_DIR=/global/cfs/cdirs/cosmo/data/dust/v0_1
+export MASK_DIR=/global/cfs/cdirs/desi/target/masks
+export GAIA_DIR=/global/cfs/cdirs/desi/target/gaia_dr2
+export URAT_DIR=/global/cfs/cdirs/desi/target/urat_dr1
+export TYCHO_DIR=/global/cfs/cdirs/desi/target/tycho_dr2
+# export TOO_DIR=/global/cfs/cdirs/desi/target/ToO
 
-# export PRIMINFO=/global/cscratch1/sd/mjwilson/secondary/sv1/outdata/0.50.0.dev4501/priminfo-dr9-0.50.0.dev4501/
-# export PRIMINFO=/global/cscratch1/sd/mjwilson/secondary/sv1/outdata/0.50.0/priminfo-dr9-0.50.0/
+export PRIMINFO=/global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/sv1/outdata/0.51.0/priminfo-dr9-0.51.0/
 
 # desienv master
-# addrepo ../desitarget (tag 0.50.0)
+# addrepo ../desitarget (tag 0.51.0)
 
-# rm -r $CSCRATCH/secondary/sv1/outdata/0.50.0.dev4501/
+# rm -r $CSCRATCH/secondary/sv1/outdata/...
 
 # select_sv_targets sweepdir dest
-srun -N 1 select_sv_targets /global/cfs/cdirs/cosmo/data/legacysurvey/dr9/south/sweep/9.0 /global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/sv1/outdata/ -s2 /global/cfs/cdirs/cosmo/data/legacysurvey/dr9/north/sweep/9.0 --nside 8 --healpixels 425 --numproc 4 --scnddir /global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/
+# srun -N 1 select_sv_targets /global/cfs/cdirs/cosmo/data/legacysurvey/dr9/south/sweep/9.0 /global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/sv1/outdata/ -s2 /global/cfs/cdirs/cosmo/data/legacysurvey/dr9/north/sweep/9.0 --nside 8 --healpixels 425 --numproc 4 --scnddir /global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/
 
-# srun -N 1 select_sv_targets /global/cfs/cdirs/cosmo/data/legacysurvey/dr9/south/sweep/9.0 $CSCRATCH -s2 /global/cfs/cdirs/cosmo/data/legacysurvey/dr9/north/sweep/9.0 --nside 8 --healpixels 427 --numproc 4 --scnddir $CSCRATCH/secondary/
+# srun -N 1 select_sv_targets /global/cfs/cdirs/cosmo/data/legacysurvey/dr9/south/sweep/9.0 /global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/sv1/outdata/ -s2 /global/cfs/cdirs/cosmo/data/legacysurvey/dr9/north/sweep/9.0 --nside 8 --healpixels 427 --numproc 4 --scnddir /global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/  
 
-# select_secondary $PRIMINFO $CSCRATCH --scnddir $CSCRATCH/secondary
+select_secondary $PRIMINFO /global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/sv1/outdata/ --scnddir /global/cscratch1/sd/mjwilson/DESILBG/GOLD/secondary/ 
 
