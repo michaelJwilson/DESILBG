6db6bf2dc4d4d08ccc40a14dbe4a403f32c3cf22
michaelJwilson <mjwilson@lbl.gov>
2021-03-11 17:45:30 -0800

    verbose bright mask.

--- gold/not_bright.py
+++ gold/not_bright.py
@@ -12,9 +12,11 @@ def not_bright(clauds):
 
         isin[is_bright] = False
 
-        print('Excluding {} bright source in {}.'.format(np.count_nonzero(is_bright), band))
+        print('Excluding {} bright source in {} [{:.2f}, {:.2f}].'.format(np.count_nonzero(is_bright), band, clauds[band][is_bright].min(), clauds[band][is_bright].max()))
 
         # More than this and we likely have a problem. 
-        # assert  np.count_nonzero(is_bright) < 20
+        # assert  np.count_nonzero(is_bright) < 1000
+
+    print('Excluding a total of {} bright source.'.format(np.count_nonzero(isin == False)))
         
     return  isin
